fec14d4790e8408798f50a781c708142bb0e3db5
ArturManuilenko <Manuilenkoav@gmail.com>
2021-02-03 17:24:19 +0300

    add absolute_url

--- lesson/models.py
+++ lesson/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Create your models here.
 
 class Material(models.Model):
-
-
     MATERIAL_TYPE = [
         ('theory', 'Theoretical material'),
         ('practice', 'Practical'),
@@ -28,12 +27,17 @@ class Material(models.Model):
                                related_name='user_materials')
 
     material_type = models.CharField(
-            max_length=25,
-            choices=MATERIAL_TYPE,
-            default='theory',
+        max_length=25,
+        choices=MATERIAL_TYPE,
+        default='theory',
     )
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
-
+    def get_absolute_url(self):  # позволяет на не зависить от того где приложение находится
+        return reverse('lesson:material_details',  # пример мы сейчас http://127.0.0.1:8000
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- lesson/urls.py
+++ lesson/urls.py
@@ -5,4 +5,8 @@ app_name = 'lesson'
 
 urlpatterns = [
     path('', views.all_materials, name='all_materials'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>',
+         views.material_details,
+         name='material_details',
+         ),
 ]

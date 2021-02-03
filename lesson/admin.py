90f74fa47870a3544830b8e3d414c1710cbbdb72
ArturManuilenko <Manuilenkoav@gmail.com>
2021-02-03 10:26:34 +0300

    rework admin

--- lesson/admin.py
+++ lesson/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.Material)
+
+# admin.site.register(models.Material) # ниже тоже самое но через декоратор и class
+@admin.register(models.Material)
+class Material(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'material_type', 'publish')
+    list_filter = ('material_type', 'created')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title', )}
+    ordering = ('material_type', 'publish')
+
 
 
